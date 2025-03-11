6ff2534967f9732c0ed3f86a17acc33448d7b229
Maftuna Avazova <maftunaavazova@shakhruhs-air.mynetworksettings.com>
2025-03-10 21:58:22 -0500

    Unicode pratice

--- unicode.js
+++ unicode.js
@@ -86,15 +86,19 @@ Assign the result to a variable named swappedString.
 //Starter Code
 // Task 1
 let inputString1 = "Code";
-let firstCodePoint; // Your code here
-let thirdCodePoint; // Your code here
+let firstCodePoint = inputString1.charCodeAt(0);
+let thirdCodePoint = inputString1.charCodeAt(2);
 
 // Task 2
-let wordFromCodePoints; // Your code here
+let wordFromCodePoints = String.fromCharCode(72, 101, 108, 108);
 
 // Task 3
 let inputString2 = "Launch";
-let swappedString; // Your code here
+let firstCharCode = inputString2.charCodeAt(0);
+let lastCharCode = inputString2.charCodeAt(inputString2.length -1);
+let firstChar = String.fromCharCode(firstCharCode);
+let lastChar = String.fromCharCode(lastCharCode);
+let swappedString = lastChar + inputString2.slice(1,-1) + firstChar;
 
 // Log all results
 console.log({
@@ -103,3 +107,4 @@ console.log({
   wordFromCodePoints,
   swappedString,
 });
+
